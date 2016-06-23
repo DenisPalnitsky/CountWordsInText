d61f66bedaef5af317787d3b51003b37d12880fa
Denis <denis.palnitsky@gmail.com>
2016-06-23 09:09:04 +0200

    Input-output

--- CountWordsInText/CountWordsInText/Program.cs
+++ CountWordsInText/CountWordsInText/Program.cs
@@ -10,6 +10,14 @@ namespace CountWordsInText
     {
         static void Main(string[] args)
         {
+            WordsCounter wordCounater = new WordsCounter();
+            var results =  wordCounater.CountWords(Console.ReadLine());
+
+            foreach (var kvp in results)
+                Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
         }
     }
 }
